aed87560cbe03c69bd3f050a38654d7ea8801861
Matthew <bergitastrophy@gmail.com>
2022-08-16 07:54:35 +0200

    Add ability to click suggestions

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 interface Props {
   clearSearch: () => void,
@@ -9,6 +9,8 @@ interface Props {
 const Input: React.FC<Props> = ({ clearSearch, initiateSearch, navigateSuggestions }) => {
   const [inputVal, setInputVal] = useState('');
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
     if (inputVal.trim().length < 3) {
       clearSearch();
@@ -18,16 +20,37 @@ const Input: React.FC<Props> = ({ clearSearch, initiateSearch, navigateSuggestio
     initiateSearch(inputVal);
   }, [inputVal]);
 
+  function handleKeydown(e: React.KeyboardEvent) {
+    if (e.code === 'Escape') {
+      if (inputRef.current !== null) inputRef.current.blur();
+      clearSearch();
+      return;
+    }
+
+    if (['ArrowUp', 'ArrowDown', 'Enter'].includes(e.code)) {
+      navigateSuggestions(e.code);
+      return;
+    }
+  }
+
   return (
     <input
+      ref={inputRef}
       type="search"
       value={inputVal}
       onChange={(e) => setInputVal(e.target.value)}
       onFocus={() => {
         if (inputVal.length >= 3) initiateSearch(inputVal);
       }}
-      onBlur={() => clearSearch()}
-      onKeyDown={(e) => navigateSuggestions(e.code)}
+      onBlur={(e) => {
+        if (e.relatedTarget?.classList.contains('github-suggestion-anchor')) {
+          const relatedTarget = e.relatedTarget as HTMLAnchorElement;
+          window.open(relatedTarget.href, '_blank');
+        }
+
+        clearSearch();
+      }}
+      onKeyDown={handleKeydown}
       className="w-full h-10 focus:bg-slate-50 focus-visible:outline-0 block px-4 sm:text-sm rounded-t-md"
       placeholder="Search GitHub..."
     />
--- components/Suggestion.tsx
+++ components/Suggestion.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Suggestion: React.FC<Props> = ({ content, url, isSelected }) => {
   return (
     <li className="h-14 border-t">
-      <a href={url} target="_blank" className="flex align-middle">
+      <a href={url} target="_blank" className="github-suggestion-anchor flex align-middle">
         <span className={`${isSelected ? 'bg-blue-600 text-white' : ''} p-4 w-full`}>{content}</span>
       </a>
     </li>
