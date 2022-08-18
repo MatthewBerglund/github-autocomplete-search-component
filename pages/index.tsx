7eeaf5032f3a9451c57fd7ca77dc1010283b8bed
Matthew <bergitastrophy@gmail.com>
2022-08-18 14:19:44 +0200

    Change folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import { useState } from 'react';
 
-import GithubSearch from '../components/GithubSearch'
+import GithubSearch from '../GithubSearch/GithubSearch'
 
 const Home: NextPage = () => {
   const [suggestions, setSuggestions] = useState<any[]>([]);
